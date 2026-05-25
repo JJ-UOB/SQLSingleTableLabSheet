eb5ed9a326fe1f386f9fd1fcddafb56805cb8902
JJ Daniel Oluwasere <ijdo20@bath.ac.uk>
2026-05-25 10:21:13 +0000

    SELECTs for questions

--- main.sql
+++ main.sql
@@ -3,6 +3,71 @@
 
 CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE);
 
-. schema
-. table
 
+INSERT INTO pet (name,owner,species,sex,checkups,birth,death)VALUES
+('Fluffy','Harold','cat','f',5,'2001-02-04',NULL),
+('Claws','Gwen','cat','m',2,'2000-03-17',NULL),
+('Buffy','Harold','dog','f',7,'1999-05-13',NULL),
+('Fang','Benny','dog','m',4,'2000-08-27',NULL),
+('Bowser','Diane','dog','m',8,'1998-08-31','2001-07-29'),
+('Chirpy','Gwen','bird','f',0,'2002-09-11',NULL),
+('Whistler','Gwen','bird','',1,'2001-12-09',NULL),
+('Slim','Benny','snake','m',5,'2001-04-29',NULL);
+
+
+SELECT * FROM pet;
+
+SELECT * FROM pet WHERE sex = "m";
+
+SELECT DISTINCT owner FROM pet;
+
+SELECT name, species, sex FROM pet WHERE species = 'snake' OR species
+= 'bird';
+
+
+
+
+-- answers to questions 1.1-6
+SELECT owner, name FROM pet WHERE sex = 'f';
+
+SELECT name, birth FROM pet WHERE species = 'dog';
+
+SELECT owner FROM pet WHERE species = 'bird';
+
+SELECT species FROM pet WHERE sex = 'f';
+
+SELECT name, birth FROM pet WHERE species = 'cat' OR species = 'bird';
+
+SELECT name, species FROM pet WHERE (species = 'cat' OR species = 'bird') AND sex = 'f';
+
+
+
+-- answers to questions 2.1-6
+SELECT owner, name FROM pet WHERE name LIKE '%er' OR name LIKE '%all';
+
+SELECT name FROM pet WHERE owner LIKE '%e%';
+
+SELECT name FROM pet WHERE name NOT LIKE '%fy';
+
+SELECT name FROM pet WHERE LENGTH(owner) = 4;
+
+SELECT DISTINCT owner FROM pet WHERE LOWER(SUBSTR(owner, 1, 1) IN ('a', 'b', 'c', 'd', 'e')
+
+SELECT DISTINCT owner FROM pet WHERE LOWER(SUBSTR(owner, -1, 1) IN ('a', 'b', 'c', 'd', 'e')
+
+
+
+-- answers to questions 3.1-7
+SELECT owner, AVG(checkups) FROM pet GROUP BY owner;
+
+SELECT species, COUNT(*) FROM pet GROUP BY species ORDER BY COUNT(*) ASC;
+
+SELECT owner, species, COUNT(*) FROM pet GROUP BY owner, species;
+
+SELECT owner, COUNT(DISTINCT species) FROM pet GROUP BY owner;
+
+SELECT sex, COUNT(*) FROM pet WHERE sex IS NOT NULL GROUP BY sex;
+
+SELECT owner, COUNT(*) FROM pet WHERE species = 'bird' GROUP BY owner;
+
+SELECT owner, SUM(checkups) FROM pet GROUP BY owner;
